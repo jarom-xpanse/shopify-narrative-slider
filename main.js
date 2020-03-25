62c6933cae06244ec1976aff65ad3e8945057948
jarom-xpanse <jarom.xpanse@gmail.com>
2020-03-25 13:03:54 +0800

    Detect keyboard Arrow Left and Right to change slide

--- main.js
+++ main.js
@@ -10,31 +10,72 @@ function init() {
   window.addEventListener("mouseout", e => {
     indicators.style.transform = "translateX(0)";
   });
+  window.addEventListener('keyup', detectKey);
 
   function showCursorLocation(e) {
     let percent = Math.round((e.pageX / window.innerWidth) * 100);
 
     let moveX = (percent - 50) * -1;
-    // console.log(moveX);
     indicators.style.transform = `translateX(${moveX}%)`;
   }
 
   slideItems = slideshow.querySelectorAll(".slide-item");
   slideItems.forEach((e, i) => {
-    console.log(e, i);
   });
 
   slideTitles = indicators.querySelectorAll("a.slide-title");
+
+    
   slideTitles.forEach((e, i) => {
     e.addEventListener("click", evt => {
-      // detect index of clicked slide
-      console.log(i);
-
-      // remove active class
-      slideItems.forEach(e => {
-        e.classList.remove("active");
-      });
-      slideItems[i].classList.add('active');
+      removeClassActive();
+      slideItems[i].classList.add("active");
     });
   });
+
+  function detectKey(e) {
+    let index = detectClassActive();
+    switch (e.key) {
+      case 'ArrowLeft':
+      if (index <= 0) return;
+      removeClassActive();
+      slideItems[index-1].classList.add("active");
+      break;
+      
+      case 'ArrowRight':
+      console.log('right');
+      if (index >= 3) return;
+      removeClassActive();
+      slideItems[index+1].classList.add("active");
+      break;
+    }
+  }
+
+  function detectClassActive() {
+    let activeIndex;
+    // this function returns index of active class removed
+    slideItems.forEach((evt, i) => {
+      evt.classList.value.split(" ").forEach(e => {
+        if (e == "active") {
+          // remove active class
+          activeIndex = i;
+        }
+      });
+    });
+    return activeIndex;
+  }
+
+  function removeClassActive() {
+    slideItems.forEach((evt, i) => {
+      evt.classList.value.split(" ").forEach(e => {
+        if (e == "active") {
+          // remove active class
+          evt.classList.remove("active");
+        }
+      });
+    });
+  }
+  
 }
+
+
