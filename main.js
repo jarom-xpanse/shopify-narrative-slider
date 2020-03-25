2da7471a207d64e360b132ba57833766d007e045
jarom-xpanse <jarom.xpanse@gmail.com>
2020-03-25 15:18:56 +0800

    slide-title underline effect when active and opacity blur when hovered

--- main.js
+++ main.js
@@ -1,53 +1,48 @@
 window.onload = init; // call the function only when the page is completely loaded
-var indicators = document.querySelector("#indicators");
-var slideTitles;
-var slideshow = document.querySelector("#slideshow");
-var slideItems;
+var indicators, slideshow, slideTitles, slideItems;
+var span;
 
 function init() {
-  // alert('hello world');
+  indicators = document.querySelector("#indicators");
+  slideshow = document.querySelector("#slideshow");
+  slideTitles = indicators.querySelectorAll("a.slide-title");
+  slideItems = slideshow.querySelectorAll(".slide-item");
+  span = indicators.querySelectorAll('span');
+
   window.addEventListener("mousemove", showCursorLocation);
-  window.addEventListener("mouseout", e => {
-    indicators.style.transform = "translateX(0)";
-  });
-  window.addEventListener('keyup', detectKey);
+  window.addEventListener("mouseout", e => indicators.style.transform = "translateX(0)");
+  window.addEventListener("keyup", detectKey);
 
   function showCursorLocation(e) {
     let percent = Math.round((e.pageX / window.innerWidth) * 100);
-
     let moveX = (percent - 50) * -1;
+    moveX /= 3;
     indicators.style.transform = `translateX(${moveX}%)`;
   }
 
-  slideItems = slideshow.querySelectorAll(".slide-item");
-  slideItems.forEach((e, i) => {
-  });
-
-  slideTitles = indicators.querySelectorAll("a.slide-title");
-
-    
   slideTitles.forEach((e, i) => {
     e.addEventListener("click", evt => {
       removeClassActive();
       slideItems[i].classList.add("active");
+      // add slide-active in slide-title span
+      span[i].classList.add('slide-active');
     });
   });
 
   function detectKey(e) {
     let index = detectClassActive();
-    switch (e.key) {
-      case 'ArrowLeft':
+    if (e.key == "ArrowLeft") {
       if (index <= 0) return;
       removeClassActive();
-      slideItems[index-1].classList.add("active");
-      break;
-      
-      case 'ArrowRight':
-      console.log('right');
+      slideItems[index - 1].classList.add("active");
+      // add slide-active in slide-title span
+      span[index-1].classList.add('slide-active');
+    } else if (e.key == "ArrowRight") {
       if (index >= 3) return;
       removeClassActive();
-      slideItems[index+1].classList.add("active");
-      break;
+      slideItems[index + 1].classList.add("active");
+      // add slide-active in slide-title span
+      span[index+1].classList.add('slide-active');
     }
   }
 
@@ -57,7 +52,6 @@ function init() {
     slideItems.forEach((evt, i) => {
       evt.classList.value.split(" ").forEach(e => {
         if (e == "active") {
-          // remove active class
           activeIndex = i;
         }
       });
@@ -69,13 +63,13 @@ function init() {
     slideItems.forEach((evt, i) => {
       evt.classList.value.split(" ").forEach(e => {
         if (e == "active") {
-          // remove active class
           evt.classList.remove("active");
+          // remove slide-active in slide-title span
+          span[i].classList.remove('slide-active');
         }
       });
     });
   }
-  
+
+
 }
-
-
